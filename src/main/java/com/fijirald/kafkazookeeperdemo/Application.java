be462a8a200a4fb180e5b778aa0cef4275052229
Sergey Strelnikov <fijirald@gmail.com>
2025-05-11 07:15:19 +0300

    Renamed KafkaDemo1ApplicationTests

--- src/main/java/com/fijirald/kafkazookeeperdemo/Application.java
+++ src/main/java/com/fijirald/kafkazookeeperdemo/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KafkaDemo1Application {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(KafkaDemo1Application.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
--- src/test/java/com/fijirald/kafkazookeeperdemo/ApplicationTests.java
+++ src/test/java/com/fijirald/kafkazookeeperdemo/ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class KafkaDemo1ApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
